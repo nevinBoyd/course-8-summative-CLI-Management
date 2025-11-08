6bf841874facd45d71ca1585516dd62b94048017
Nevin <n.boyd1@yahoo.com>
2025-11-07 21:40:31 -0800

    add task relationship methods to Project

--- models/project.py
+++ models/project.py
@@ -14,6 +14,18 @@ class Project:
         self.description = description
         self.due_date = due_date
         self.tasks = []  # One-to-many: Project -> Tasks
+    
+    def add_task(self, task):
+        """
+        Add a Task instance to this project's task list.
+        """
+        self.tasks.append(task)
 
+    def list_tasks(self):
+        """
+        Return a list of the tasks for this project.
+        """
+        return self.tasks
+    
     def __str__(self):
         return f"Project(title='{self.title}', due_date='{self.due_date}')"
